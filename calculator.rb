690b99972ba56d386e60bc89f8b54f46ad77ca4b
Adeja Lendor <alendor6@gmail.com>
2019-08-25 16:06:50 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
-# Add your variables here
+def calculator(first_number, second_number)
+  first_number = Integer || Float
+  second_number = Integer || Float
+  second_number != 0
+  sum = first_number + second_number
+  print sum
+end
+calculator(3, 2)
