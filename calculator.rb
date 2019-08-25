7d009ed21005d071421ec49d7eeace2c8709947d
Adeja Lendor <alendor6@gmail.com>
2019-08-25 16:22:28 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,7 @@
-def calculator(first_number, second_number)
-  first_number = Integer || Float
-  second_number = Integer || Float
-  second_number != 0
+  first_number = 8
+  second_number = 12
   sum = first_number + second_number
-  print sum
-end
-calculator(3, 2)
+  difference = first_number - second_number
+  product = first_number * second_number
+  quotient = first_number / second_number
+  
